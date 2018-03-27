92e7729801475391f6b89d5bb34edb7ea5edbf2b
DBockrath <dbockrath26@fairport.org>
2018-03-26 20:32:00 -0400

    Secured connection data so it is not public

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,36 +1,73 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) {
 
-        String serverName = args[0];
-        int port = 25565;
+        String rName = null;
+        String dName = null;
+        int port = 0;
 
         try {
 
-            System.out.println("Connecting to " + serverName + " on port " + port);
+            Scanner scanner = new Scanner(new File("C:\\Users\\diboc\\Documents\\IntelliJ Workspace\\ComputerVisionAPI\\Client\\ServerData"));
+//            Scanner scanner = new Scanner(new File(""));
+
+
+            rName = scanner.nextLine();
+            dName = scanner.nextLine();
+            port = Integer.parseInt(scanner.nextLine());
+
+
+        } catch (IOException e) {
+
+            System.out.println("Error reading from file");
+
+        }
+
+        try {
+
+            String serverName = rName;
+
             Socket client = new Socket(serverName, port);
 
-            System.out.println("Just connected to " + client.getRemoteSocketAddress());
+            System.out.println("Connected to Server 1");
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
 
-            out.writeUTF("Hello from " + client.getLocalSocketAddress());
+            out.writeUTF("Hello from Server 1");
             InputStream inFromServer = client.getInputStream();
             DataInputStream in = new DataInputStream(inFromServer);
 
-            System.out.println("Server says " + in.readUTF());
-            //establish connection
-
-
-
+            System.out.println("Server 1 says " + in.readUTF());
             client.close();
 
         } catch (IOException e) {
 
-            e.printStackTrace();
+            try {
+
+                String serverName = dName;
+
+                Socket client = new Socket(serverName, port);
+
+                System.out.println("Connected to Server 2");
+                OutputStream outToServer = client.getOutputStream();
+                DataOutputStream out = new DataOutputStream(outToServer);
+
+                out.writeUTF("Hello from Server 2");
+                InputStream inFromServer = client.getInputStream();
+                DataInputStream in = new DataInputStream(inFromServer);
+
+                System.out.println("Server 2 says " + in.readUTF());
+                client.close();
+
+            } catch (IOException er) {
+
+                System.out.println("Could not connect to Server 1 or 2");
+
+            }
 
         }
 
