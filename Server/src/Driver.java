a3d84ac711aa46c0b2fd8c5b74a8521c2abb43ea
strychnos12 <maggie49>
2018-04-11 21:32:46 -0400

    debug stuff, CM is about 10 to 20% more effiecent (in general) when comapared to an arraylist plus it is sorted

--- Server/src/Driver.java
+++ Server/src/Driver.java
@@ -1,10 +1,39 @@
+import CatMatrix.ListNode;
 import CatMatrix.ListNodeMatrix;
+
+import java.util.ArrayList;
 
 public class Driver {
 
     public static void main(String[] args) {
-        ListNodeMatrix LNM = new ListNodeMatrix();
 
+     /*   ArrayList<ListNode> list = new ArrayList<>();
+        for(int k = 0; k < 7500000; k++)
+        {
+            list.add(new ListNode(k, "" + k,null));
+        } */
+
+
+        ListNodeMatrix LNM = new ListNodeMatrix();
+        for(int i = 0; i < 1; i++)
+        {
+            LNM.addMain("" + i);
+            for(int k = 0; k < 5000; k++)
+                {
+                LNM.addSub(k, "" + k, i);
+            }
+        }
+
+       // LNM.saveData();
+       // LNM.printAll();
+
+
+
+        System.out.println("Finished!");
+        while(true)
+        {
+
+        }
     }
 
 }
