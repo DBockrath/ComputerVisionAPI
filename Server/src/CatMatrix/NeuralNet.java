385c1d5b576e8d4846bd8587af2b6d3d2a9f2de3
strychnos12 <maggie49>
2018-05-14 14:10:14 -0400

    Added methods and goals to the NeuralNet

--- Server/src/CatMatrix/NeuralNet.java
+++ Server/src/CatMatrix/NeuralNet.java
@@ -1,8 +1,22 @@
 package CatMatrix;
 
 import NeuralNetwork.BitString;
+import NeuralNetwork.NeuralNetwork;
 
 public class NeuralNet extends ListNodeMatrix{
+
+    NeuralNetwork checkDev;
+    NeuralNetwork catsNet;
+    NeuralNetwork subNet;
+
+    private double perDev = 50;
+    private double numCat = 100; //should be moved to LNM
+
+    private void check()
+    { //goes through the entire database and move objects to fit a the current dev
+        perDev = Math.pow(67.05910855, numCat);
+
+    }
 
     public NeuralNet()
     {
@@ -11,7 +25,10 @@ public class NeuralNet extends ListNodeMatrix{
         System.out.println("yes");
     }
 
-    public void inputImages(BitString in, String name, String cat)
+    //goals
+    //input images, it adds an image to LNM and trains the nueral net with this image
+
+    public void inputImages(BitString in, String name)
     {
 
     }
@@ -23,5 +40,7 @@ public class NeuralNet extends ListNodeMatrix{
 
         return null;
     }
+
+
 
 }
