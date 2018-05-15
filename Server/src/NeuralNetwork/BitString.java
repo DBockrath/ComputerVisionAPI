8020144dd46ed0bde307fa89e971c403b3896f37
Dan Bockrath <dbockrath26@fairport.org>
2018-05-15 18:42:54 -0400

    Organized BitString more

--- Server/src/NeuralNetwork/BitString.java
+++ Server/src/NeuralNetwork/BitString.java
@@ -7,22 +7,16 @@ import java.util.regex.Pattern;
 public class BitString {
 
     private ArrayList<Character> bits;
-
-    private final int rawImagePixels = 230400;
     private final int numOfPixels = 57600;
 
     public BitString(String text) {
 
         bits = new ArrayList<>(text.length());
 
-        for (int i = 0; i < text.length(); i++) {
+        for (int i = 0; i < text.length(); i++) bits.add(text.charAt(i));
 
-            bits.add(text.charAt(i));
-
-        }
-
-        if (isCompressed()) decompress();
-        if (bits.size() == rawImagePixels) resize();
+        decompress();
+        if (bits.size() != numOfPixels) resize();
         compress();
 
     }
@@ -31,164 +25,35 @@ public class BitString {
 
         bits = new ArrayList<>((int)Math.pow(image.length, 2));
 
-        for (int row = 0; row < image.length; row++) {
+        for (int[] anImage : image)
+            for (int column = 0; column < image[0].length; column++)
+                bits.add((char) anImage[column]);
 
-            for (int col = 0; col < image[0].length; col++) {
-
-                bits.add((char)image[row][col]);
-
-            }
-
-        }
-
-        if (isCompressed()) decompress();
-        if (bits.size() == rawImagePixels) resize();
+        decompress();
+        if (bits.size() != numOfPixels) resize();
         compress();
 
     }
 
-    private void compress() {
+    public BitString(int length) {
 
-        Pattern pattern = Pattern.compile("0+|1+");
-        Matcher matcher = pattern.matcher(convertToString());
+        bits = new ArrayList<>(length);
 
-        ArrayList<Character> bitsCopy = new ArrayList<>();
-
-        while (matcher.find()) {
-
-            if (matcher.group(0).length() < 16) {
-
-                if (matcher.group(0).startsWith("1")) {
-
-                    bitsCopy.add('1');
-                    bitsCopy.add(getExtension(matcher.group(0).length()));
-
-                } else if (matcher.group(0).startsWith("0")) {
-
-                    bitsCopy.add('0');
-                    bitsCopy.add(getExtension(matcher.group(0).length()));
-
-                }
-
-            } else if (matcher.group(0).length() >= 16) {
-
-                if (matcher.group(0).startsWith("1")) {
-
-                    bitsCopy.add('1');
-                    bitsCopy.add(getExtension(15));
-
-                    int leftover = matcher.group(0).length() - 15;
-
-                    while (leftover >= 16) {
-
-                        bitsCopy.add('1');
-                        bitsCopy.add(getExtension(15));
-                        leftover -= 15;
-
-                    }
-
-                    bitsCopy.add('1');
-                    bitsCopy.add(getExtension(leftover));
-
-                } else if (matcher.group(0).startsWith("0")) {
-
-                    bitsCopy.add('0');
-                    bitsCopy.add(getExtension(15));
-
-                    int leftover = matcher.group(0).length() - 15;
-
-                    while (leftover >= 16) {
-
-                        bitsCopy.add('0');
-                        bitsCopy.add(getExtension(15));
-                        leftover -= 15;
-
-                    }
-
-                    bitsCopy.add('0');
-                    bitsCopy.add(getExtension(leftover));
-
-                }
-
-            }
-
-        }
-
-        bits = bitsCopy;
-
-    }
-
-    private void decompress() {
-
-        ArrayList<Character> bitsCopy = new ArrayList<>();
-
-        for (int i = 0; i < bits.size() - 1; i += 2) {
-
-            int length = 0;
-
-            switch (bits.get(i + 1)) {
-
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    length = Character.getNumericValue(bits.get(i + 1));
-                    break;
-
-                case 'A':
-                    length = 10;
-                    break;
-
-                case 'B':
-                    length = 11;
-                    break;
-
-                case 'C':
-                    length = 12;
-                    break;
-
-                case 'D':
-                    length = 13;
-                    break;
-
-                case 'E':
-                    length = 14;
-                    break;
-
-                case 'F':
-                    length = 15;
-                    break;
-
-            }
-
-            for (int j = 1; j <= length; j++) {
-
-                bitsCopy.add(bits.get(i));
-
-            }
-
-        }
-
-        bits = bitsCopy;
+        for (int i = 0; i < bits.size(); i++)
+            bits.set(i, '0');
 
     }
 
     private void resize() {
 
-        if (isCompressed()) decompress();
+        decompress();
 
         ArrayList<Character> newBits = new ArrayList<>(numOfPixels);
         int[][] matrix = new int[(int)Math.sqrt(numOfPixels)][(int)Math.sqrt(numOfPixels)];
-        int i = 0;
 
         for (int row = 0; row < matrix.length; row++) {
 
-            if (row != 0) i += 478;
+            int i = row * 478;
 
             for (int col = 0; col < matrix[0].length; col++) {
 
@@ -200,20 +65,82 @@ public class BitString {
         }
 
         bits = newBits;
+        compress();
 
     }
 
-    private String convertToString() {
+    private void compress() {
 
-        StringBuilder builder = new StringBuilder(bits.size());
+        if (!isCompressed()) {
 
-        for (Character ch: bits) {
+            Pattern pattern = Pattern.compile("0+|1+");
+            Matcher matcher = pattern.matcher(getCurrentString());
 
-            builder.append(ch);
+            ArrayList<Character> bitsCopy = new ArrayList<>();
+
+            while (matcher.find()) {
+
+                if (matcher.group(0).length() < 16) {
+
+                    if (matcher.group(0).startsWith("1")) {
+
+                        bitsCopy.add('1');
+                        bitsCopy.add(getExtension(matcher.group(0).length()));
+
+                    } else if (matcher.group(0).startsWith("0")) {
+
+                        bitsCopy.add('0');
+                        bitsCopy.add(getExtension(matcher.group(0).length()));
+
+                    }
+
+                } else if (matcher.group(0).length() >= 16) {
+
+                    if (matcher.group(0).startsWith("1")) {
+
+                        bitsCopy.add('1');
+                        bitsCopy.add(getExtension(15));
+
+                        int leftover = matcher.group(0).length() - 15;
+
+                        while (leftover >= 16) {
+
+                            bitsCopy.add('1');
+                            bitsCopy.add(getExtension(15));
+                            leftover -= 15;
+
+                        }
+
+                        bitsCopy.add('1');
+                        bitsCopy.add(getExtension(leftover));
+
+                    } else if (matcher.group(0).startsWith("0")) {
+
+                        bitsCopy.add('0');
+                        bitsCopy.add(getExtension(15));
+
+                        int leftover = matcher.group(0).length() - 15;
+
+                        while (leftover >= 16) {
+
+                            bitsCopy.add('0');
+                            bitsCopy.add(getExtension(15));
+                            leftover -= 15;
+
+                        }
+
+                        bitsCopy.add('0');
+                        bitsCopy.add(getExtension(leftover));
+
+                    }
+
+                }
+
+            }
+
+            bits = bitsCopy;
 
         }
-
-        return builder.toString();
 
     }
 
@@ -265,6 +192,123 @@ public class BitString {
 
     }
 
+    private void decompress() {
+
+        if (isCompressed()) {
+
+            ArrayList<Character> bitsCopy = new ArrayList<>();
+
+            for (int i = 0; i < bits.size() - 1; i += 2) {
+
+                int length = 0;
+
+                switch (bits.get(i + 1)) {
+
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        length = Character.getNumericValue(bits.get(i + 1));
+                        break;
+
+                    case 'A':
+                        length = 10;
+                        break;
+
+                    case 'B':
+                        length = 11;
+                        break;
+
+                    case 'C':
+                        length = 12;
+                        break;
+
+                    case 'D':
+                        length = 13;
+                        break;
+
+                    case 'E':
+                        length = 14;
+                        break;
+
+                    case 'F':
+                        length = 15;
+                        break;
+
+                }
+
+                for (int j = 1; j <= length; j++) bitsCopy.add(bits.get(i));
+
+            }
+
+            bits = bitsCopy;
+
+        }
+
+    }
+
+    public boolean isCompressed() {
+
+        String text = getCurrentString();
+
+        return (text.contains("2") || text.contains("3") || text.contains("4") || text.contains("5") || text.contains("6") || text.contains("7") || text.contains("8") || text.contains("9")
+                || text.contains("A") || text.contains("B") || text.contains("C") || text.contains("D") || text.contains("E") || text.contains("F"));
+
+    }
+
+    public void deleteBits() {
+
+        bits = null;
+
+    }
+
+    public double calcPercentDeviation(BitString other) {
+
+        decompress();
+        other.decompress();
+
+        int numOfDeviations = 0;
+        int size = bits.size();
+
+        for (int i = 0; i < size; i++) {
+
+            if (bits.get(1) != other.get(i)) numOfDeviations += 1;
+
+        }
+
+        compress();
+        other.compress();
+        return (size - numOfDeviations) / size;
+
+    }
+
+    public int Length() {
+
+        decompress();
+
+        int size = bits.size();
+
+        compress();
+        return size;
+
+    }
+
+    public char get(int pos) {
+
+        decompress();
+
+        char bit = bits.get(pos);
+
+        compress();
+        return bit;
+
+    }
+
     public void set(int pos, char val) {
 
         decompress();
@@ -283,19 +327,16 @@ public class BitString {
 
     public int[][] getImage() {
 
+        decompress();
+
         int[][] image = new int[(int)Math.sqrt(bits.size())][(int)Math.sqrt(bits.size())];
         int i = 0;
 
-        for (int row = 0; row < image.length; row++) {
+        for (int row = 0; row < image.length; row++)
+            for (int column = 0; column < image[0].length; column++)
+                image[row][column] = Character.getNumericValue(bits.get(i++));
 
-            for (int col = 0; col < image[0].length; col++) {
-
-                image[row][col] = Character.getNumericValue(bits.get(i++));
-
-            }
-
-        }
-
+        compress();
         return image;
 
     }
@@ -315,89 +356,53 @@ public class BitString {
 
     public double[] getDoubleArray() {
 
-        boolean wasCompressed = false;
-
-        if (isCompressed()) {
-
-            decompress();
-            wasCompressed = true;
-
-        }
+        decompress();
 
         String string = toString();
         double[] returnArray = new double[string.length()];
 
-        for (int i = 0; i < string.length(); i++) {
-
+        for (int i = 0; i < string.length(); i++)
             returnArray[i] = string.charAt(i);
 
-        }
-
-        if (wasCompressed) compress();
+        compress();
         return returnArray;
 
     }
 
-    public boolean isCompressed() {
-
-        String text = convertToString();
-
-        return (text.contains("2") || text.contains("3") || text.contains("4") || text.contains("5") || text.contains("6") || text.contains("7") || text.contains("8") || text.contains("9")
-                || text.contains("A") || text.contains("B") || text.contains("C") || text.contains("D") || text.contains("E") || text.contains("F"));
-
-    }
-
-    public int Length() {
-
-        if (isCompressed()) decompress();
-        return bits.size();
-
-    }
-
-    public char getBit(int pos) {
-
-        if (isCompressed()) decompress();
-        return bits.get(pos);
-
-    }
-
-    public void delete() {
-
-        bits = null;
-
-    }
-
-    public double calcPercentDeviation(BitString other) {
-
-        if (isCompressed()) decompress();
-        if (other.isCompressed()) other.decompress();
-
-        int numOfDeviations = 0;
-
-        for (int i = 0; i < bits.size(); i++) {
-
-            if (bits.get(1) != other.getBit(i)) numOfDeviations += 1;
-
-        }
-
-        return (bits.size() - numOfDeviations) / bits.size();
-
-    }
-
-    public String toString() {
-
-        if (isCompressed()) decompress();
+    public String getCurrentString() {
 
         StringBuilder builder = new StringBuilder(bits.size());
 
-        for (Character ch: bits) {
-
+        for (Character ch: bits)
             builder.append(ch);
 
-        }
+        return builder.toString();
 
-        if (!isCompressed()) compress();
+    }
 
+    public String getCompressedString() {
+
+        compress();
+
+        StringBuilder builder = new StringBuilder(bits.size());
+
+        for (Character ch: bits)
+            builder.append(ch);
+
+        return builder.toString();
+
+    }
+
+    public String getDecompressedString() {
+
+        decompress();
+
+        StringBuilder builder = new StringBuilder(bits.size());
+
+        for (Character ch: bits)
+            builder.append(ch);
+
+        compress();
         return builder.toString();
 
     }
