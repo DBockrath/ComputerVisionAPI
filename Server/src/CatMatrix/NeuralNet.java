df081a678abcdb1e99fb49af9001d06213b46bf3
Dan Bockrath <dbockrath26@fairport.org>
2018-05-15 21:31:13 -0400

    Added a few methods and organized tasks to complete.

--- Server/src/CatMatrix/NeuralNet.java
+++ Server/src/CatMatrix/NeuralNet.java
@@ -1,16 +1,18 @@
 package CatMatrix;
 
 import NeuralNetwork.BitString;
-import NeuralNetwork.NeuralNetwork;
+import NeuralNetwork.Matrix;
 
-public class NeuralNet extends ListNodeMatrix{
+public class NeuralNet extends ListNodeMatrix {
 
-    NeuralNetwork checkDeviationNetwork;
-    NeuralNetwork categoriesNetwork;
-    NeuralNetwork subNetwork;
+    /*
+    TODO: Check over new methods to make sure they make sense
+    TODO: Make body of inputImages(), getName(), and setCategories()
+     */
 
+    private Matrix categoryWeightMatrix;
+    private final int numOfNeurons = 57600;
     private double minPercentDeviation = 50;
-    private double numberOfCategories = 100; // Should be moved to LNM
 
     public NeuralNet() {
 
@@ -23,16 +25,126 @@ public class NeuralNet extends ListNodeMatrix{
     // Goals
     // inputImages adds an image to LNM and trains the neural net with this image
     // decideCategory will calc the deviation percentage between a cat and an input
+    // Each category will be and average of the sub objects. Use calcAverageBitString() to do this
 
-    private String decideCategory(BitString in, int cats) {
+    private void trainCategoryWeightMatrix() throws Exception {
 
-        // Goes through the entire database and move objects to fit a the current dev
-        minPercentDeviation = Math.pow(67.05910855, cats);
+        categoryWeightMatrix = new Matrix(numOfNeurons, numOfNeurons);
+
+        for (int cat = 0; cat < numCats(); cat++) {
+
+            BitString input = (BitString) super.getMainValue(cat);
+            double[] bipolarInput = input.getBipolarArray();
+            Matrix bipolarMatrix = Matrix.toRowMatrix(bipolarInput);
+            Matrix transposeBipolarMatrix = bipolarMatrix.transpose();
+            Matrix multiplyMatrix = transposeBipolarMatrix.multiply(bipolarMatrix);
+            Matrix subtractMatrix = multiplyMatrix.subtract(Matrix.identity(categoryWeightMatrix.getData().length));
+            categoryWeightMatrix = categoryWeightMatrix.add(subtractMatrix);
+
+        }
+
+    }
+
+    private BitString runCategoryNetwork(BitString input) {
+
+        BitString output = new BitString(input.Length());
+        Matrix bipolarMatrix = Matrix.toRowMatrix(input.getBipolarArray());
+
+        for (int column = 0; column < input.Length(); column++) {
+
+            try {
+
+                Matrix columnMatrix = categoryWeightMatrix.getColumnMatrix(column);
+                double dotProductResult = bipolarMatrix.dotProduct(columnMatrix);
+
+                if (dotProductResult > 0) {
+
+                    output.set(column, '1');
+
+                } else {
+
+                    output.set(column, '0');
+
+                }
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+        return output;
+
+    }
+
+    private BitString runSubNetwork(BitString input, int category) throws Exception {
+
+        Matrix subWeightMatrix = new Matrix(numOfNeurons, numOfNeurons);
+
+        for (int pos = 0; pos < super.numSubNodes(category); pos++) {
+
+            BitString subObject = (BitString) super.getSubObject(category, pos);
+            double[] bipolarInput = subObject.getBipolarArray();
+            Matrix bipolarMatrix = Matrix.toRowMatrix(bipolarInput);
+            Matrix transposeBipolarMatrix = bipolarMatrix.transpose();
+            Matrix multiplyMatrix = transposeBipolarMatrix.multiply(bipolarMatrix);
+            Matrix subtractMatrix = multiplyMatrix.subtract(Matrix.identity(subWeightMatrix.getData().length));
+            subWeightMatrix = subWeightMatrix.add(subtractMatrix);
+
+        }
+
+        BitString output = new BitString(input.Length());
+        Matrix bipolarMatrix = Matrix.toRowMatrix(input.getBipolarArray());
+
+        for (int column = 0; column < input.Length(); column++) {
+
+            try {
+
+                Matrix columnMatrix = subWeightMatrix.getColumnMatrix(column);
+                double dotProductResult = bipolarMatrix.dotProduct(columnMatrix);
+
+                if (dotProductResult > 0) {
+
+                    output.set(column, '1');
+
+                } else {
+
+                    output.set(column, '0');
+
+                }
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+        return output;
+
+    }
+
+    private String decideCategory(BitString input, int numOfNodes) {
+
+        BitString output = runCategoryNetwork(input);
+
+        minPercentDeviation = Math.pow(67.05910855, numOfNodes);
         String category = null;
+        double bestDeviation = .5;
 
-        for (int cat = 1; cat <= super.numNodes(); cat++) {
+        for (int cat = 0; cat < numOfNodes; cat++) {
 
-//            in.calcPercentDeviation(super.get);
+            double deviation = output.calcPercentDeviation((BitString) super.getMainValue(cat));
+
+            if (deviation > bestDeviation) {
+
+                bestDeviation = deviation;
+                category = super.getMainName(cat);
+
+            }
 
         }
 
@@ -40,11 +152,16 @@ public class NeuralNet extends ListNodeMatrix{
 
     }
 
+    private void setCategories() {
+
+        // Sets the categories with the average BitString of each sub object
+
+    }
+
     public void inputImages(BitString in, String name) {
 
-        // Creates new neural nets that have as many neurons as bits in BitString in
-        categoriesNetwork = new NeuralNetwork(in.Length());
-        subNetwork = new NeuralNetwork(in.Length());
+        // Creates new weight matrices that have as many neurons as bits in BitString in
+
 
     }
 
